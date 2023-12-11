c37673f4788a5970c5374086cd44b8e4906c7d43
raccoon-cai <daniel_cai@brown.edu>
2023-12-11 00:29:23 -0500

    Audio v3

--- src/Config/ParamsControl.js
+++ src/Config/ParamsControl.js
@@ -4,7 +4,8 @@ import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js'
 let parameters = { 
     timeOfDay: 18.432, 
     autorun: false,
-    snowSpeed: 1
+    snowSpeed: 1,
+    music: true
  }
 
 export function setupControlPanel(snowglobe) { 
@@ -17,6 +18,7 @@ export function setupControlPanel(snowglobe) {
     gui.add(parameters, 'timeOfDay', 0, 24)
     gui.add(parameters, 'autorun')
     gui.add(parameters, 'snowSpeed', 0.5, 3)
+    gui.add(parameters, 'music')
 
     // gui.add(parameters, 'zoom', 0.025, 2)
 
--- src/main.js
+++ src/main.js
@@ -392,25 +392,27 @@ document.addEventListener("keyup", function(event)
 });
 
 // AUDIO
-var listener = new THREE.AudioListener();
-camera.add(listener);
+if (snowglobe.params.music) {
+  var listener = new THREE.AudioListener();
+  camera.add(listener);
 
-var sound = new THREE.Audio(listener);
-var audioLoader = new THREE.AudioLoader();
-var isAudioPlaying = false;
+  var sound = new THREE.Audio(listener);
+  var audioLoader = new THREE.AudioLoader();
+  var isAudioPlaying = false;
 
-function loadAndPlayAudio() {
-    audioLoader.load('../song.mp3', function (buffer) {
-        sound.setBuffer(buffer);
-        sound.setLoop(true);
-        sound.setVolume(0.2);
+  function loadAndPlayAudio() {
+      audioLoader.load('../song.mp3', function (buffer) {
+          sound.setBuffer(buffer);
+          sound.setLoop(true);
+          sound.setVolume(0.2);
 
-        if (!isAudioPlaying) {
-            sound.play();
-            isAudioPlaying = true;
-        }
-    });
+          if (!isAudioPlaying) {
+              sound.play();
+              isAudioPlaying = true;
+          }
+      });
+  }
+
+  loadAndPlayAudio();
 }
-
-loadAndPlayAudio();
 
